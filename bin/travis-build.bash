98140d88518e9efbd388be5dd54ece5422220956
kmbn <github@nevermindtheumlauts.com>
2018-02-02 16:31:32 +0100

    Build CKAN with Travis

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -1,32 +1,60 @@
+#!/bin/bash
 set -e
 
-echo "This is travis-install-dependencies..."
+echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
-sudo apt-get install postgresql-$PGVERSION solr-jetty
+sudo apt-get update -qq
+sudo apt-get install solr-jetty libcommons-fileupload-java
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-git checkout "ckan-2.7.2"
+if [ $CKANVERSION == 'master' ]
+then
+    echo "CKAN version: master"
+else
+    CKAN_TAG=$(git tag | grep ^ckan-$CKANVERSION | sort --version-sort | tail -n 1)
+    git checkout $CKAN_TAG
+    echo "CKAN version: ${CKAN_TAG#ckan-}"
+fi
 python setup.py develop
-pip install -r requirements.txt
-pip install -r dev-requirements.txt
-pip install coveralls
+sed -i -e 's/psycopg2==2.4.5/psycopg2==2.7.3.2/g' requirements.txt
+pip install -r requirements.txt --allow-all-external
+sed -i -e 's/pycodestyle==2.2.0/# pycodestyle==2.2.0/g' dev-requirements.txt
+pip install -r dev-requirements.txt --allow-all-external
 cd -
+
+echo "Setting up Solr..."
+# solr is multicore for tests on ckan master now, but it's easier to run tests
+# on Travis single-core still.
+# see https://github.com/ckan/ckan/issues/2972
+sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
+printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty restart
 
 echo "Creating the PostgreSQL user and database..."
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
+sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
+sudo -u postgres psql -c 'CREATE DATABASE datastore_test WITH OWNER ckan_default;'
 
 echo "Initialising the database..."
 cd ckan
 paster db init -c test-core.ini
 cd -
 
-echo "Installing ckanext-kpis and its requirements..."
-python setup.py develop
-pip install -r requirements.txt
-pip install -r dev-requirements.txt
+echo "Install other libraries required..."
+sudo apt-get install python-dev
 
-echo "travis-install-dependencies is done."
+echo "Installing ckanext-kpis and its requirements..."
+pip install -r requirements.txt
+
+python setup.py develop
+
+echo "Moving test.ini into a subdir..."
+mkdir subdir
+mv test-core.ini subdir
+
+echo "travis-build.bash is done."
